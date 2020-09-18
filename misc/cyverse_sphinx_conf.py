251aa3dbef3412346d3d4748435a37c2c5ce9ae5
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-18 10:43:55 -0400

    add triage issue template

--- misc/cyverse_sphinx_conf.py
+++ misc/cyverse_sphinx_conf.py
@@ -1,3 +1,5 @@
+# Version 2.0, May 2020
+
 # -*- coding: utf-8 -*-
 
 import sys
@@ -9,11 +11,11 @@ extensions = [
     'sphinx.ext.autodoc',
 ]
 
-project = 'Evaluate High-throughput Sequencing Reads with FastQC Quickstart'
-copyright = '2017, CyVerse'
+project = 'YOUR PROJECT NAME HERE'
+copyright = '2020, CyVerse'
 author = 'CyVerse'
-version = '1.0'
-release = '1.0'
+version = '2.0'
+release = '2.0'
 
 language = None
 source_parsers = {
@@ -71,6 +73,10 @@ def setup(app):
     )
     app.add_transform(AutoStructify)
     app.add_stylesheet('cyverse.css')
+    app.add_stylesheet('detail-expand.css')
+    app.add_stylesheet('question-answer.css')
     #uncomment to enable table sorting app.add_javascript('jquery.tablesorter.min.js')
     app.add_javascript('cyverse.js')
+    app.add_javascript('detail-expand.js')
+    app.add_javascript('question-answer.js')
     app.add_javascript('intercom-script-for-learning.js')
